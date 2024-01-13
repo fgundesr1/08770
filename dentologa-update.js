641e9b6d0021ae2b3ccb7a031eb555c4f64bedc3
fgundesr1 <104692064+fgundesr1@users.noreply.github.com>
2024-01-13 14:17:32 -0300

    Update dentologa-update.js

--- dentologa-update.js
+++ dentologa-update.js
@@ -1,20 +1,6 @@
 // rodar script se a url possuir a palavra "ok"
 
-(function(d, t) {
-    var BASE_URL = "https://chat.plugoo.com.br";
-    var g = d.createElement(t),
-        s = d.getElementsByTagName(t)[0];
-    g.src = BASE_URL + "/packs/js/sdk.js";
-    g.defer = true;
-    g.async = true;
-    s.parentNode.insertBefore(g, s);
-    g.onload = function() {
-        window.chatwootSDK.run({
-            websiteToken: 'yBEbbFxSZ9zYsacWmnkpFdQv',
-            baseUrl: BASE_URL
-        })
-    }
-})(document, "script");
+
 
 
 if ($(".pagina-produto").length && typeof tabelaMedidas !== "undefined") {
@@ -130,21 +116,25 @@ setTimeout(function() {
 setTimeout(function() {
     if ($(".conteudo-topo .menu-user-welcome").length <= 0) {
         var alterarUser = $(".conteudo-topo .btn-group .pequeno.dropdown-toggle").text().trim().split(" ")[1];
-        $(".my_account_info .my_account-title").text(`OlÃ¡, ${alterarUser}`);
+        $(".my_account_info .my_account-title").text(`Olá, ${alterarUser}`);
         $(".my_account_info .my_account-subtitle").html(`<a href="/conta/index">Minha Conta</a>`);
         if (window.screen.width <= 768) {
-            $(".lrMenu .minhaconta").addClass("exit").removeClass("minhaconta")
+            $(".lrMenu .minhaconta").addClass("exit").removeClass("minhaconta");
             $(".lrMenu .exit div").html(`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
-  <path fill-rule="evenodd" d="M7.5 3.75A1.5 1.5 0 006 5.25v13.5a1.5 1.5 0 001.5 1.5h6a1.5 1.5 0 001.5-1.5V15a.75.75 0 011.5 0v3.75a3 3 0 01-3 3h-6a3 3 0 01-3-3V5.25a3 3 0 013-3h6a3 3 0 013 3V9A.75.75 0 0115 9V5.25a1.5 1.5 0 00-1.5-1.5h-6zm5.03 4.72a.75.75 0 010 1.06l-1.72 1.72h10.94a.75.75 0 010 1.5H10.81l1.72 1.72a.75.75 0 11-1.06 1.06l-3-3a.75.75 0 010-1.06l3-3a.75.75 0 011.06 0z" clip-rule="evenodd" />
-</svg>
-`)
+                <path fill-rule="evenodd" d="M7.5 3.75A1.5 1.5 0 006 5.25v13.5a1.5 1.5 0 001.5 1.5h6a1.5 1.5 0 001.5-1.5V15a.75.75 0 011.5 0v3.75a3 3 0 01-3 3h-6a3 3 0 01-3-3V5.25a3 3 0 013-3h6a3 3 0 013 3V9A.75.75 0 0115 9V5.25a1.5 1.5 0 00-1.5-1.5h-6zm5.03 4.72a.75.75 0 010 1.06l-1.72 1.72h10.94a.75.75 0 010 1.5H10.81l1.72 1.72a.75.75 0 11-1.06 1.06l-3-3a.75.75 0 010-1.06l3-3a.75.75 0 011.06 0z" clip-rule="evenodd" />
+            </svg>`);
+
             $(".lrMenu .meuspedidos").before(`<li class="minhaconta">
-			      <div class="icon-options-menu-mobile">
-						<svg class="user-icon" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve"><path d="M418.651,431.634c-8.576-56.229-45.769-103.945-98.194-125.989c42.679-35.547,48.457-98.962,12.91-141.641s-98.962-48.457-141.641-12.91s-48.457,98.962-12.91,141.641c3.904,4.69,8.229,9.006,12.91,12.91c-49.783,21.12-86.007,65.344-96.914,118.309c-2.021,9.893,4.37,19.557,14.263,21.577c9.893,2.021,19.557-4.37,21.577-14.263c14.153-69.257,81.774-113.938,151.04-99.785c52.407,10.706,92.626,52.873,100.846,105.728c1.408,8.978,9.198,15.561,18.286,15.451h2.834C413.595,450.981,420.306,441.582,418.651,431.634z M192,228.571c0-35.346,28.654-64,64-64s64,28.654,64,64s-28.654,64-64,64S192,263.918,192,228.571z"></path><path d="M256,512C114.615,512,0,397.385,0,256S114.615,0,256,0s256,114.615,256,256S397.385,512,256,512z M256,36.571c-121.189,0-219.429,98.24-219.429,219.429S134.811,475.429,256,475.429S475.429,377.189,475.429,256S377.189,36.571,256,36.571z"></path><g></g></svg>
-			      </div>
-			   <a href="/conta/index">Minha Conta</a>
-			</li>`)
-            $(".lrMenu .faleconosco-mobile").after($(".lrMenu .exit"))
+                <div class="icon-options-menu-mobile">
+                    <svg class="user-icon" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
+                        <path d="M418.651,431.634c-8.576-56.229-45.769-103.945-98.194-125.989c42.679-35.547,48.457-98.962,12.91-141.641s-98.962-48.457-141.641-12.91s-48.457,98.962-12.91,141.641c3.904,4.69,8.229,9.006,12.91,12.91c-49.783,21.12-86.007,65.344-96.914,118.309c-2.021,9.893,4.37,19.557,14.263,21.577c9.893,2.021,19.557-4.37,21.577-14.263c14.153-69.257,81.774-113.938,151.04-99.785c52.407,10.706,92.626,52.873,100.846,105.728c1.408,8.978,9.198,15.561,18.286,15.451h2.834C413.595,450.981,420.306,441.582,418.651,431.634z M192,228.571c0-35.346,28.654-64,64-64s64,28.654,64,64s-28.654,64-64,64S192,263.918,192,228.571z"></path>
+                        <path d="M256,512C114.615,512,0,397.385,0,256S114.615,0,256,0s256,114.615,256,256S397.385,512,256,512z M256,36.571c-121.189,0-219.429,98.24-219.429,219.429S134.811,475.429,256,475.429S475.429,377.189,475.429,256S377.189,36.571,256,36.571z"></path>
+                        <g></g>
+                    </svg>
+                </div>
+                <a href="/conta/index">Minha Conta</a>
+            </li>`);
+            $(".lrMenu .faleconosco-mobile").after($(".lrMenu .exit"));
         }
     }
 
@@ -152,18 +142,14 @@ setTimeout(function() {
 
 $(document).ready(function() {
     var interval = setInterval(function() {
-        let tagEmbedCss = $('link[href="https://widget.tagembed.com/widget/static/css/react-build.css"]')
-        // se tagEmbedCss nÃ£o for encontrado, adicionar script tagEmbeb
+        let tagEmbedCss = $('link[href="https://widget.tagembed.com/widget/static/css/react-build.css"]');
         if (tagEmbedCss.length) {
             tagEmbedCss[0].disabled = true;
-            console.log('Plugoo: Removed unnecessary CSS from TagEmbed')
+            console.log('Plugoo: Removed unnecessary CSS from TagEmbed');
             clearInterval(interval);
         } else {
             console.log('Plugoo: TagEmbed CSS not found');
 
-            // $('#corpo').append('<div id="instagram-feed-foxtech"></div>');
-
-            // instagram update 28-06-2021
             if (typeof instagramFeed !== 'undefined') {
                 if (instagramFeed.ativa === true) {
                     $('#corpo').append('<div id="insta-feed"><div class="conteiner row-fluid"><p class="instagram_username">Siga no instagram<a href="https://www.instagram.com/' + instagramFeed.usuario + '/" rel="noopener" target="_blank">@' + instagramFeed.usuario + '</a></p></div><div id="instagram-feed"><div class="tagembed-container" style="width: 100%;height: 100%;overflow: auto;"><div class="tagembed-socialwall" data-wall-id="' + instagramFeed.id + '" view-url="https://widget.tagembed.com/' + instagramFeed.id + '?view"></div></div></div></div>');
@@ -185,30 +171,27 @@ $(document).ready(function() {
 });
 
 // FullBanners
-if (typeof resolutionBanner !== 'undefined') {
-    if (resolutionBanner.condition === true) {
-        var fBanner = $('.full-banners');
-        if (fBanner.length) {
-            // for em cada imagem do banner
-            fBanner.find('img').each(function() {
-                // pega o src da imagem
-                var src = $(this).attr('src');
-                // cria um novo elemento de imagem
-                var img = $('<img />');
-                // adiciona o src da imagem no novo elemento
-                img.attr('src', src);
-                // replace da qualidade da imagem de 1140x1140 por 1920x1920
-                img.attr('src', img.attr('src').replace('1140x1140', '1920x1920'));
-                // trocar a imagem antiga pela nova em cada banner
-                $(this).replaceWith(img);
-            });
-        }
+if (typeof resolutionBanner !== 'undefined' && resolutionBanner.condition === true) {
+    var fBanner = $('.full-banners');
+    if (fBanner.length) {
+        // for em cada imagem do banner
+        fBanner.find('img').each(function() {
+            // pega o src da imagem
+            var src = $(this).attr('src');
+            // cria um novo elemento de imagem
+            var img = $('<img />');
+            // adiciona o src da imagem no novo elemento
+            img.attr('src', src);
+            // replace da qualidade da imagem de 1140x1140 por 1920x1920
+            img.attr('src', img.attr('src').replace('1140x1140', '1920x1920'));
+            // trocar a imagem antiga pela nova em cada banner
+            $(this).replaceWith(img);
+        });
     }
 }
 
 window.onload = function() {
-    if ("undefined" != typeof horarioAtendimento) {
-
+    if (typeof horarioAtendimento !== 'undefined') {
         $('#rodape .contato .operation').html(
             '<li class="complement">' +
             '<li class="operation" id="operation"><span class="titulo">' + horarioAtendimento.titulo + '</span><p>' + horarioAtendimento.horarios + '</p></li>' +
@@ -220,57 +203,52 @@ window.onload = function() {
 // NOVO CDN - AJUSTES NO MINHA CONTA
 $('.my_account ul').hide();
 
-newAccountDesktop = function() {
-    
-	const svgNewAccountDesk = `<svg class="user-icon" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve"><path d="M418.651,431.634c-8.576-56.229-45.769-103.945-98.194-125.989c42.679-35.547,48.457-98.962,12.91-141.641s-98.962-48.457-141.641-12.91s-48.457,98.962-12.91,141.641c3.904,4.69,8.229,9.006,12.91,12.91c-49.783,21.12-86.007,65.344-96.914,118.309c-2.021,9.893,4.37,19.557,14.263,21.577c9.893,2.021,19.557-4.37,21.577-14.263c14.153-69.257,81.774-113.938,151.04-99.785c52.407,10.706,92.626,52.873,100.846,105.728c1.408,8.978,9.198,15.561,18.286,15.451h2.834C413.595,450.981,420.306,441.582,418.651,431.634z M192,228.571c0-35.346,28.654-64,64-64s64,28.654,64,64s-28.654,64-64,64S192,263.918,192,228.571z"></path><path d="M256,512C114.615,512,0,397.385,0,256S114.615,0,256,0s256,114.615,256,256S397.385,512,256,512z M256,36.571c-121.189,0-219.429,98.24-219.429,219.429S134.811,475.429,256,475.429S475.429,377.189,475.429,256S377.189,36.571,256,36.571z"></path><g></g></svg>` 	
- 
-	$(".my_account").html(`${svgNewAccountDesk}
+function newAccountDesktop() {
+    const svgNewAccountDesk = `<svg class="user-icon" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve"><path d="M418.651,431.634c-8.576-56.229-45.769-103.945-98.194-125.989c42.679-35.547,48.457-98.962,12.91-141.641s-98.962-48.457-141.641-12.91s-48.457,98.962-12.91,141.641c3.904,4.69,8.229,9.006,12.91,12.91c-49.783,21.12-86.007,65.344-96.914,118.309c-2.021,9.893,4.37,19.557,14.263,21.577c9.893,2.021,19.557-4.37,21.577-14.263c14.153-69.257,81.774-113.938,151.04-99.785c52.407,10.706,92.626,52.873,100.846,105.728c1.408,8.978,9.198,15.561,18.286,15.451h2.834C413.595,450.981,420.306,441.582,418.651,431.634z M192,228.571c0-35.346,28.654-64,64-64s64,28.654,64,64s-28.654,64-64,64S192,263.918,192,228.571z"></path><path d="M256,512C114.615,512,0,397.385,0,256S114.615,0,256,0s256,114.615,256,256S397.385,512,256,512z M256,36.571c-121.189,0-219.429,98.24-219.429,219.429S134.811,475.429,256,475.429S475.429,377.189,475.429,256S377.189,36.571,256,36.571z"></path><g></g></svg>`;
+
+    $(".my_account").html(`${svgNewAccountDesk}
     <div class="my_account_info">
-    	<a href="/conta/login" class="my_account-title">
-    		<div class="newUser">
-    			<span class="userName">OlÃ¡, bem vindo(a)<span>
-    		</div>
- 
-    	</a>
-    <span class="my_account-subtitle">
-		            <a href="/conta/login">Entre</a> 
-		            ou 
-		            <a href="/conta/login">Cadastre-se</a>
-		    </span>
-		        </div>
+        <a href="/conta/login" class="my_account-title">
+            <div class="newUser">
+                <span class="userName">Olá, bem-vindo(a)<span>
+            </div>
+        </a>
+        <span class="my_account-subtitle">
+            <a href="/conta/login">Entre</a> 
+            ou 
+            <a href="/conta/login">Cadastre-se</a>
+        </span>
+    </div>
     <ul>
         <li class="listAccount"><a href="/conta/pedido/listar">Meus pedidos</a></li>
         <li class="listAccount"><a href="/conta/favorito/listar">Meus favoritos</a></li>
         <li class="shipping"><a href="/#modalRastreio" data-toggle="modal" data-target="#modalRastreio">Acompanhar pedido</a></li>
- 
-    </ul>`)
- 
- 
-	if ($.cookie("LI-UserLoggedName") != '' && "undefined" != typeof $.cookie("LI-UserLoggedName")) {
+    </ul>`);
+
+    if ($.cookie("LI-UserLoggedName") != '' && typeof $.cookie("LI-UserLoggedName") !== "undefined") {
         $(".my_account_info").html(`<a href="/conta/login" class="my_account-title">
-    		<div class="newUser">
-    			<span class="userName">OlÃ¡, <b>${$.cookie("LI-UserLoggedName").trim().split(" ")[0]}</b></span>
-    			</div>
-    	</a>
-    	<span class="my_account-subtitle">
-		            <a href="/conta/index">Acessar Conta</a> 
-		</span>`)
-        $(".my_account ul").append(`<li class="login"><a href="/conta/logout">Sair</a></li>`)
-    }
-	else if (typeof $.cookie("LI-UserLoggedName") == "undefined" && $(".btn-group .botao.secundario.pequeno").text() != '') {
-    const nomeUpd = $(".btn-group .botao.secundario.pequeno").text().trim().split(" ")[1]
+            <div class="newUser">
+                <span class="userName">Olá, <b>${$.cookie("LI-UserLoggedName").trim().split(" ")[0]}</b></span>
+            </div>
+        </a>
+        <span class="my_account-subtitle">
+            <a href="/conta/index">Acessar Conta</a> 
+        </span>`);
+        $(".my_account ul").append(`<li class="login"><a href="/conta/logout">Sair</a></li>`);
+    } else if (typeof $.cookie("LI-UserLoggedName") === "undefined" && $(".btn-group .botao.secundario.pequeno").text() !== '') {
+        const nomeUpd = $(".btn-group .botao.secundario.pequeno").text().trim().split(" ")[1];
         $(".my_account_info").html(`<a href="/conta/login" class="my_account-title">
-    		<div class="newUser">
-    			<span class="userName">OlÃ¡, <b>${nomeUpd}</b></span>
-    			</div>
-    	</a>
-    	<span class="my_account-subtitle">
-		            <a href="/conta/index">Acessar Conta</a> 
-		</span>`)
-        $(".my_account ul").append(`<li class="login"><a href="/conta/logout">Sair</a></li>`)
+            <div class="newUser">
+                <span class="userName">Olá, <b>${nomeUpd}</b></span>
+            </div>
+        </a>
+        <span class="my_account-subtitle">
+            <a href="/conta/index">Acessar Conta</a> 
+        </span>`);
+        $(".my_account ul").append(`<li class="login"><a href="/conta/logout">Sair</a></li>`);
     }
- 
 }
+
  
 newAccountMobile = function() {
 	const newSvgExit = `<div class="icon-options-menu-mobile"><svg xmlns="http://www.w3.org/2000/svg" viewBox="-17 0 512 512"><path d="m462.453125 247.742188h-165.164063c-9.121093 0-16.515624-7.386719-16.515624-16.515626 0-9.128906 7.394531-16.515624 16.515624-16.515624h165.164063c9.121094 0 16.515625 7.386718 16.515625 16.515624 0 9.128907-7.394531 16.515626-16.515625 16.515626zm0 0"></path><path d="m412.902344 297.289062c-4.226563 0-8.453125-1.609374-11.675782-4.839843-6.453124-6.449219-6.453124-16.902344 0-23.351563l37.871094-37.871094-37.871094-37.871093c-6.453124-6.453125-6.453124-16.90625 0-23.355469 6.453126-6.449219 16.902344-6.449219 23.355469 0l49.546875 49.550781c6.453125 6.449219 6.453125 16.902344 0 23.351563l-49.546875 49.546875c-3.226562 3.230469-7.453125 4.839843-11.679687 4.839843zm0 0"></path><path d="m363.355469 165.160156c9.121093 0 16.515625-7.386718 16.515625-16.515625v-132.128906c0-9.128906-7.394532-16.515625-16.515625-16.515625h-346.839844c-.753906 0-1.394531.332031-2.121094.429688-.976562.128906-1.875.265624-2.8125.566406-1.417969.449218-2.65625 1.113281-3.875 1.90625-.5.324218-1.109375.398437-1.578125.773437-.207031.167969-.285156.4375-.484375.613281-1.226562 1.09375-2.15625 2.410157-3.015625 3.824219-.339844.5625-.816406 1.015625-1.089844 1.621094-.949218 2.078125-1.539062 4.347656-1.539062 6.78125v429.417969c0 7.792968 5.445312 14.515625 13.054688 16.144531l231.226562 49.550781c1.144531.257813 2.304688.371094 3.460938.371094 3.742187 0 7.417968-1.273438 10.386718-3.675781 3.878906-3.132813 6.128906-7.855469 6.128906-12.839844v-33.03125h99.097657c9.121093 0 16.515625-7.386719 16.515625-16.519531v-132.128906c0-9.128907-7.394532-16.515626-16.515625-16.515626-9.121094 0-16.515625 7.386719-16.515625 16.515626v115.613281h-82.582032v-363.351563c0-7.792968-5.445312-14.515625-13.054687-16.144531l-78.808594-16.890625h174.445313v115.613281c0 9.128907 7.394531 16.515625 16.515625 16.515625zm-132.128907 309.890625-198.195312-42.46875v-395.632812l198.195312 42.46875zm0 0"></path></svg></div>`
